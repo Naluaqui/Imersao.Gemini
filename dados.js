88664181f2705e5a8e8adced3616980f38fdd03c
Ana Luiza <analu.tibi@outlook.com>
2024-09-06 18:10:57 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,13 @@
+let dados = [
+    {titulo : "Dom Casmurro",
+    descricao : "Dom Casmurro, obra-prima de Machado de Assis, nos convida a uma profunda reflexão sobre a natureza humana, o amor, a traição e a construção da memória.",
+    link : "https://drive.google.com/file/d/0B5ng3u9zmYByTm9JbW5EVlV1bUE/view?usp=sharing&resourcekey=0-pe506TBbLdb2ifU6oWniJg"},
+        
+    {titulo : "O Pequeno Príncipe",
+    descricao : "O Pequeno Príncipe é uma obra literária do escritor francês Antoine de Saint-Exupéry, que conta a história da amizade entre um homem frustrado por ninguém compreender os seus desenhos, com um principezinho que habita um asteroide no espaço.",
+    link : "https://seu_link_para_o_pdf_do_pequeno_principe"},
+            
+    {titulo : "Tartarugas até lá embaixo",
+    descricao : "A história acompanha a jornada de Aza Holmes, uma menina de 16 anos que sai em busca de um bilionário misteriosamente desaparecido – quem encontrá-lo receberá uma polpuda recompensa em dinheiro – enquanto tenta lidar com o próprio transtorno obsessivo-compulsivo (TOC).",
+    link : "https://seu_link_para_o_pdf_de_tartarugas"}
+    ]
